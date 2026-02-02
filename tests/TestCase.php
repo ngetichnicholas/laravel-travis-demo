b27dd035659145e4877ffd6f61a16f9f43099530
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2026-02-02 09:31:44 +0300

    Add Travis CI config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function test_homepage_loads()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
 }
